9d9ed846ca6bdc6e270f6a072b9c57f14fb4535f
Ekweaga <hikay133@gmail.com>
2023-11-02 00:42:44 +0100

    login/signup done

--- pages/authentication/login.js
+++ pages/authentication/login.js
@@ -3,41 +3,67 @@
 import React from 'react'
 import Link from "next/link"
 import {useState,useEffect} from "react"
+import React from 'react'
+   import { supabase } from '@/superbaseConfig'
+    import {
+        FormControl,
+        FormLabel,
+        FormErrorMessage,
+        FormHelperText,
+      } from '@chakra-ui/react'
+    
+      import { Button, ButtonGroup } from '@chakra-ui/react'
 
 function Login() {
 const [email,setEmail] = useState("")
 const [password,setPassword] = useState("")
-  const loginInUser = ()=>{
-
+  const loginInUser = async()=>{
+    try{
+      const { data, error } = await supabase.auth.signUp({
+        email: 'example@email.com',
+        password: 'example-password',
+      })
+    }
+    catch(error){
+      
+    }
+   
   }
   return (
-    <div className='flex items-center justify-center text-white'>
-    <div className='  h-[500px] w-[350px] mt-[100px] rounded-md  p-4 flex items-center justify-center flex-col bg-[#1D4645] shadow-2xl' >
-      <h1 className='font-bold text-2xl mb-[10px]'>Welcome back!</h1>
-      <p>Please login to continue</p>
-      <form className='flex flex-col gap-[30px] items-center justify-center mt-[15px]' onSubmit={loginInUser}>
-        <div>
-          <input type="email" placeholder='Email' className='bg-transparent text-white border-[1px] border-white w-[300px] px-2 h-[40px]' value={email} onChange={(e)=>setEmail(e.target.value)}/>
+   
+    
+  
+        <>
+         <FormControl>
+          <FormLabel>Email</FormLabel>
+          <Input type='email' value={input} onChange={handleInputChange} />
+          {!isError ? (
+            <FormHelperText>
+              Enter the email you'd like to receive the newsletter on.
+            </FormHelperText>
+          ) : (
+            <FormErrorMessage>Email is required.</FormErrorMessage>
+          )}
+        </FormControl>
+        <FormControl>
+          <FormLabel>Email</FormLabel>
+          <Input type='password' value={input} onChange={handleInputChange} />
+          {!isError ? (
+            <FormHelperText>
+              Enter the email you'd like to receive the newsletter on.
+            </FormHelperText>
+          ) : (
+            <FormErrorMessage>Email is required.</FormErrorMessage>
+          )}
+        </FormControl>
+        <Button colorScheme='blue'>Button</Button>
+        
+        </>
+      )
+    }
+    
+        
+  
 
-        </div>
-
-        <div>
-        <input type="password" placeholder='your password' className='bg-transparent text-white border-[1px] border-white w-[300px] px-2 h-[40px]' value={password} onChange={(e)=>setPassword(e.target.value)}/>
-        </div>
-        <div>
-        <button className="bg-[#E8C5B0] text-black w-[280px] rounded-full p-2">Get Started</button>
-        </div>
-        <div className="text-white text-sm">
-          <p>Dont have an account ? <Link href="/views/Signup" className='text-[#14f2fe] font-bold'>SignUp</Link></p>
-        </div>
-        <div className="text-white text-sm">
-          <p><Link href="/">Go back home</Link></p>
-        </div>
-      </form>
-
-    </div>
-   </div>
-  )
-}
 
 export default Login
--- pages/authentication/signup.js
+++ pages/authentication/signup.js
@@ -1,9 +1,50 @@
 import React from 'react'
 import { supabase } from '@/superbaseConfig'
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+  } from '@chakra-ui/react'
+
+  import { Button, ButtonGroup } from '@chakra-ui/react'
 
 function Signup() {
+
+    const submit = async()=>{
+        const { data, error } = await supabase.auth.signUp({
+            email: 'example@email.com',
+            password: 'example-password',
+          })
+
+    }
   return (
-    <div>Signup</div>
+    <>
+     <FormControl>
+      <FormLabel>Email</FormLabel>
+      <Input type='email' value={input} onChange={handleInputChange} />
+      {!isError ? (
+        <FormHelperText>
+          Enter the email you'd like to receive the newsletter on.
+        </FormHelperText>
+      ) : (
+        <FormErrorMessage>Email is required.</FormErrorMessage>
+      )}
+    </FormControl>
+    <FormControl>
+      <FormLabel>Email</FormLabel>
+      <Input type='password' value={input} onChange={handleInputChange} />
+      {!isError ? (
+        <FormHelperText>
+          Enter the email you'd like to receive the newsletter on.
+        </FormHelperText>
+      ) : (
+        <FormErrorMessage>Email is required.</FormErrorMessage>
+      )}
+    </FormControl>
+    <Button colorScheme='blue'>Button</Button>
+    
+    </>
   )
 }
 
