ecc72d43b3f3897a0faa55294cf8338d2f16cc53
Ekweaga <hikay133@gmail.com>
2024-01-08 15:21:22 +0100

    pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-onst withPWA = require('next-pwa')({
+const withPWA = require('next-pwa')({
   dest: 'public',
   register: true, // Register the PWA service worker
   skipWaiting: true, // Skip waiting for service worker activation
